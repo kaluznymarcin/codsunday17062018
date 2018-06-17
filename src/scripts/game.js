780b4a179e4a2c97e71aa71e760fd208ac5697fd
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2018-06-17 15:53:18 +0200

    zajecia 3

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,12 +1,30 @@
 (function () {
+    let score = 0;
     const gameElement = document.createElement("div");
     const panelElement = document.createElement("div");
     const arenaElement = document.createElement("div");
 
     const moleElement = document.createElement("div");
 
-    moleElement.style.top = "50px";
-    moleElement.style.left = "50px";
+    let timeUID = null;
+
+    arenaElement.addEventListener(
+        "click",
+        function (e) {
+            const element = e.target;
+            e.stopPropagation();
+
+            if (moleElement === element) {
+                score += 1;
+                panelElement.innerHTML = score;
+                showMole();
+            }
+        },
+        false
+    );
+
+    showMole();
+    panelElement.innerHTML = score;
 
     gameElement.classList.add("game");
     panelElement.classList.add("panel");
@@ -19,7 +37,20 @@
         arenaElement
     );
 
+    const list = Math.floor(Math.random() * 10);
+
     arenaElement.appendChild(moleElement);
 
     document.body.appendChild(gameElement);
+
+    function changePosition() {
+        moleElement.style.top = `${Math.floor(Math.random() * 10) * 50}px`;
+        moleElement.style.left = (Math.floor(Math.random() * 10) * 50 + "px");
+    }
+
+    function showMole() {
+        clearInterval(timeUID);
+        changePosition()
+        timeUID = setInterval(changePosition, 1 * 30 * 1000);
+    }
 }());
